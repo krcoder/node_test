769d8bbfe188cdc7385900e79e35937fcd121dff
Min Kyu, Kang <flwkemwlsxl@gmail.com>
2024-05-29 21:43:53 +0900

    #1
    first node test

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+ 
+app.use(cors())
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+  
+  app.listen(9001)
